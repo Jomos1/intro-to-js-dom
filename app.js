d17a506c51560bd08c17e18a9bbe4d3a7eadad5c
Student <idk123@gmail.com>
2017-03-13 08:28:59 -1000

    modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("san check")
+//Finding HTML Elements
+// console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+var tagElement = document.getElementsByTagName("script");
+console.log(tagElement);
