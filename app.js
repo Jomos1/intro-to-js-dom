e5007a1585a4a52630e6d0d40e438ec5218a51e2
Student <idk123@gmail.com>
2017-03-13 08:33:39 -1000

    modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ var idElement = document.getElementById("use-id-method");
 console.log(idElement);
 var tagElement = document.getElementsByTagName("script");
 console.log(tagElement);
+console.log("i slept for 1 hour, pls help")
+console.log(document.getElementsByClassName("use-class-method"));
