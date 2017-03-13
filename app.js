b94f83b109b33aec0b435c273482aa17069567d8
Student <idk123@gmail.com>
2017-03-13 08:44:42 -1000

    modified:   app.js

--- app.js
+++ app.js
@@ -7,3 +7,9 @@ var tagElement = document.getElementsByTagName("script");
 console.log(tagElement);
 console.log("i slept for 1 hour, pls help")
 console.log(document.getElementsByClassName("use-class-method"));
+//changing html elements
+console.log(idElement.innerHTML);
+idElement.innerHTML = "I have been asleep for ten decades";
+
+console.log(classElements[1]);
+classElements[1].innerHTML = "I have been changed by className and array target";
